bbcf5420932d008c9d151e88032c4a0df55b4240
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2025-06-12 10:29:18 -0400

    sphincs128f-mldsa44 chain

--- pico/src/tls_client.c
+++ pico/src/tls_client.c
@@ -11,7 +11,7 @@
 
 #define SLEEP_MS (100)
 
-#include "pico-pqtls/cafiles/mldsa44.h"
+#include "pico-pqtls/cafiles/sphincs128f-mldsa44-mldsa44-mldsa44.h"
 #if !defined(AUTH_SUITE) || !defined(CA_CERT)
 #error "AUTH_SUITE or CA_CERT missing"
 #endif
