aeed67d3d40c4991616ded0c0a96216df0a6984b
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2025-06-12 21:12:41 -0400

    more data

--- pico/src/tls_client.c
+++ pico/src/tls_client.c
@@ -12,7 +12,9 @@
 #define SLEEP_MS (100)
 
 // #include "pico-pqtls/cafiles/rsa2048-rsa2048-rsa2048.h"
-#include "pico-pqtls/cafiles/sha256ecdsa.h"
+// #include "pico-pqtls/cafiles/sha256ecdsa.h"
+// #include "pico-pqtls/cafiles/ed25519.h"
+#include "pico-pqtls/cafiles/mldsa44.h"
 // #include "pico-pqtls/cafiles/sphincs128f-mldsa44-mldsa44-mldsa44.h"
 #if !defined(AUTH_SUITE) || !defined(CA_CERT)
 #error "AUTH_SUITE or CA_CERT missing"
@@ -22,14 +24,14 @@
 
 static ntp_client_t ntp_client;
 
-#define KEX_NAME "secp256r1"
+#define KEX_NAME "mlkem512"
 static int kex_groups[] = {
-    WOLFSSL_ECC_SECP256R1,
+    // WOLFSSL_ECC_SECP256R1,
     // WOLFSSL_ECC_SECP384R1,
     // WOLFSSL_ECC_SECP521R1,
     // WOLFSSL_ECC_X25519,
     // WOLFSSL_ECC_X448,
-    // WOLFSSL_ML_KEM_512,
+    WOLFSSL_ML_KEM_512,
     // WOLFSSL_ML_KEM_768,
     // WOLFSSL_ML_KEM_1024
     // HQC_128,
