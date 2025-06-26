676a6bf27addd6af1798bebaf3c17ba51b2172a8
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2025-06-26 18:47:09 -0400

    sphincs192s-sphincs192s-hqc192 chain

--- pico/src/tls_client.c
+++ pico/src/tls_client.c
@@ -24,7 +24,8 @@
 // #include "pico-pqtls/cafiles/sphincs128s-mldsa44-mldsa44.h"
 // #include "pico-pqtls/cafiles/mldsa44-mldsa44-mlkem512-mldsa44.h"
 // #include "pico-pqtls/cafiles/mldsa44-mldsa44-hqc128-mldsa44.h"
-#include "pico-pqtls/cafiles/falcon1024-falcon1024-mlkem1024.h"
+// #include "pico-pqtls/cafiles/falcon1024-falcon1024-mlkem1024.h"
+#include "pico-pqtls/cafiles/sphincs192s-sphincs192s-hqc192.h"
 // #include "pico-pqtls/cafiles/sphincs256s-sphincs256s-mlkem1024.h"
 #if !defined(AUTH_SUITE) || !defined(CA_CERT)
 #error "AUTH_SUITE or CA_CERT missing"
@@ -32,7 +33,7 @@
 
 static ntp_client_t ntp_client;
 
-#define KEX_NAME "mlkem1024"
+#define KEX_NAME "hqc192"
 static int kex_groups[] = {
     // WOLFSSL_ECC_SECP256R1,
     // WOLFSSL_ECC_SECP384R1,
@@ -41,9 +42,9 @@ static int kex_groups[] = {
     // WOLFSSL_ECC_X448,
     // WOLFSSL_ML_KEM_512,
     // WOLFSSL_ML_KEM_768,
-    WOLFSSL_ML_KEM_1024,
+    // WOLFSSL_ML_KEM_1024,
     // HQC_128,
-    // HQC_192,
+    HQC_192,
     // HQC_256,
     // OT_ML_KEM_512,
     // OT_ML_KEM_768,
