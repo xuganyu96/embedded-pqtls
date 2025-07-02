b35566baa10cb9bf3830365829d7a66491b5c1fb
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2025-07-02 19:56:18 -0400

    falcon1024-falcon1024-mldsa87 data; falcon1024 chain

--- server/src/certgen.c
+++ server/src/certgen.c
@@ -61,10 +61,10 @@ static enum Ctc_SigType int_sig_type = CTC_FALCON_LEVEL5;
 /* ML-KEM-512 OID: 2.16.840.1.101.3.4.4.1
  * HQC-128 OID: 2.16.840.1.101.3.4.4.4
  * ML-DSA-44 OID: 2.16.840.1.101.3.4.3.17 */
-static enum CertType leaf_key_type = ML_DSA_LEVEL5_TYPE;
-static enum Ctc_SigType leaf_sig_type = CTC_ML_DSA_LEVEL5;
-static enum CertType client_key_type = ML_DSA_LEVEL5_TYPE;
-static enum Ctc_SigType client_sig_type = CTC_ML_DSA_LEVEL5;
+static enum CertType leaf_key_type = FALCON_LEVEL5_TYPE;
+static enum Ctc_SigType leaf_sig_type = CTC_FALCON_LEVEL5;
+static enum CertType client_key_type = FALCON_LEVEL5_TYPE;
+static enum Ctc_SigType client_sig_type = CTC_FALCON_LEVEL5;
 
 static void set_certname(CertName *cert_name, const char *country,
                          const char *state, const char *locality,
