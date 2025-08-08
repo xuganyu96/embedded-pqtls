d3c0c1a3b555a17c0ede565c1034444eed96e1b0
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2025-08-08 13:38:03 -0400

    CliArgs for client

--- server/src/tlsclient.c
+++ server/src/tlsclient.c
@@ -1,104 +1,156 @@
-#include <netdb.h>
+/* tlsclient.c
+ *
+ * A simple TLS client that performs a handshake then immediately hang up
+ *
+ * Command line options:
+ *  --debug         enables wolfssl_debugging_on, requires DEBUG_WOLFSSL at
+ *                  compile time
+ *  --cafile <path> load PEM-encoded root certificate. If supplied, then server
+ *                  authentication is required, else it is disabled
+ *  --certs <path>  load certificate chain.
+ *  --key <path>    load private key
+ *  <host>          remote host to connect to
+ *  <port>          remote port to connect to
+ */
+#include <inttypes.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 
 #include "wolfssl/ssl.h"
 
-#define HTTP_REQUEST                                                           \
-    "GET /octocat HTTP/1.1\r\n"                                                \
-    "Host: api.github.com\r\n"                                                 \
-    "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:136.0) "     \
-    "Gecko/20100101 Firefox/136.0\r\n"                                         \
-    "Accept: application/json\r\n"                                             \
-    "Connection: close\r\n\r\n"
+#define HELP                                                                   \
+    "Usage: tlsclient [--debug] [--cafile <path>] [--certs <path>] [--key "    \
+    "<path>] <host> <port>"
 
-/**
- * Establishes a TCP connection to a specified hostname and port.
- *
- * Creates a socket, resolves the hostname to an IP address,
- * and attempts to connect to the specified port on the resolved address.
- *
- * @param hostname The hostname to connect to (e.g., "example.com").
- * @param port The port number to connect to (e.g., 80 for HTTP).
- * @return int Returns the socket file descriptor on success, or -1 on failure.
- */
-static int tcp_connect(const char *hostname, int port) {
-    struct sockaddr_in server_addr; /* netinet/in.h */
-    struct hostent *server;         /* netdb.h */
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        return -1;
+/* Return true if path points to a regular file */
+static int is_file(const char *path) {
+    struct stat target;
+    int err;
+    if ((err = stat(path, &target)) != 0) {
+        fprintf(stderr, "Cannot read from %s\n", path);
+        return 0;
     }
-
-    server = gethostbyname(hostname);
-    if (!server) {
-        return -1;
-    }
-
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-    memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
-
-    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) <
-        0) {
-        close(sockfd); /* unistd.h */
-        return -1;
-    }
-    return sockfd;
+    return S_ISREG(target.st_mode);
 }
 
-int main(void) {
-    wolfSSL_Init();
+struct CliArgs {
+    int debug;
+    char *cafile;
+    char *certfile;
+    char *keyfile;
+    char *host;
+    uint16_t port;
+};
 
-    unsigned char http_rx_buf[2048];
-    int rx_buf_len = 0, readlen;
+int CliArgs_init(struct CliArgs *args) {
+    if (args == NULL) {
+        return BAD_FUNC_ARG;
+    }
+    memset(args, 0, sizeof(struct CliArgs));
+    return 0;
+}
 
-    int ssl_ret, sockfd;
-    WOLFSSL *ssl;
-    WOLFSSL_CTX *ctx;
-
-    if ((ctx = wolfSSL_CTX_new(wolfTLSv1_3_client_method())) == NULL) {
-        fprintf(stderr, "Failed to create WolfSSL context\n");
-        exit(EXIT_FAILURE);
+int CliArgs_check(struct CliArgs *args) {
+    if (args->cafile != NULL && !is_file(args->cafile)) {
+        fprintf(stderr, "%s is not valid file\n", args->cafile);
+        return -1;
     }
-    wolfSSL_CTX_set_verify(ctx, WOLFSSL_VERIFY_NONE, NULL);
-    if ((ssl = wolfSSL_new(ctx)) == NULL) {
-        fprintf(stderr, "Failed to create SSL struct\n");
-        wolfSSL_CTX_free(ctx);
-        exit(EXIT_FAILURE);
+    if (args->certfile != NULL && !is_file(args->certfile)) {
+        fprintf(stderr, "%s is not valid file\n", args->certfile);
+        return -1;
     }
-    if ((sockfd = tcp_connect("api.github.com", 443)) < 0) {
-        fprintf(stderr, "Failed to establish TCP connection\n");
-        wolfSSL_free(ssl);
-        wolfSSL_CTX_free(ctx);
-        exit(EXIT_FAILURE);
+    if (args->keyfile != NULL && !is_file(args->keyfile)) {
+        fprintf(stderr, "%s is not valid file\n", args->keyfile);
+        return -1;
     }
-    wolfSSL_set_fd(ssl, sockfd);
-    if ((ssl_ret = wolfSSL_connect(ssl)) != SSL_SUCCESS) {
-        close(sockfd);
-        wolfSSL_free(ssl);
-        wolfSSL_CTX_free(ctx);
-        exit(EXIT_FAILURE);
+    if ((args->port < 1024) || (args->port > 65535)) {
+        fprintf(stderr,
+                "port must be between 1024 and 65535, got %" PRIu16 "\n",
+                args->port);
+        return -1;
     }
-    printf("Connected to github.com:443\n");
-
-    wolfSSL_write(ssl, HTTP_REQUEST, strlen(HTTP_REQUEST));
-    while ((readlen = wolfSSL_read(ssl, http_rx_buf + rx_buf_len,
-                                   sizeof(http_rx_buf) - rx_buf_len)) > 0) {
-        rx_buf_len += readlen;
-    }
-    http_rx_buf[rx_buf_len] = '\0';
-    printf("%s\n", http_rx_buf);
-
-    wolfSSL_shutdown(ssl);
-    close(sockfd);
-    wolfSSL_free(ssl);
-    ssl = NULL;
-    wolfSSL_CTX_free(ctx);
-    ctx = NULL;
-    wolfSSL_Cleanup();
 
     return 0;
+}
+
+int CliArgs_parse(struct CliArgs *args, int argc, char **argv) {
+    int argi = 1; /* argv[0] is program name */
+    int expect_kwargs = argi < argc;
+
+    while (expect_kwargs && argi < argc) {
+        if (strncmp(argv[argi], "--debug", sizeof("--debug")) == 0) {
+            args->debug = 1;
+            argi++;
+        } else if (strncmp(argv[argi], "--cafile", sizeof("--cafile")) == 0) {
+            if (argi + 1 >= argc) {
+                fprintf(stderr, "Missing value for --cafile\n");
+                return -1;
+            }
+            args->cafile = argv[argi + 1];
+            argi += 2;
+        } else if (strncmp(argv[argi], "--certs", sizeof("--certs")) == 0) {
+            if (argi + 1 >= argc) {
+                fprintf(stderr, "Missing value for --certs\n");
+                return -1;
+            }
+            args->certfile = argv[argi + 1];
+            argi += 2;
+        } else if (strncmp(argv[argi], "--key", sizeof("--key")) == 0) {
+            if (argi + 1 >= argc) {
+                fprintf(stderr, "Missing value for --key\n");
+                return -1;
+            }
+            args->keyfile = argv[argi + 1];
+            argi += 2;
+        } else {
+            expect_kwargs = 0;
+        }
+    }
+
+    if (argi < argc) {
+        args->host = argv[argi++];
+    }
+    if (argi < argc) {
+        args->port = atoi(argv[argi]);
+    }
+
+    return CliArgs_check(args);
+}
+
+int CliArgs_debug(struct CliArgs *args) {
+    if (args->debug) {
+        printf("%12s: %d\n", "debug", args->debug);
+    }
+    if (args->cafile) {
+        printf("%12s: %s\n", "cafile", args->cafile);
+    }
+    if (args->certfile) {
+        printf("%12s: %s\n", "certs", args->certfile);
+    }
+    if (args->keyfile) {
+        printf("%12s: %s\n", "keyfile", args->keyfile);
+    }
+    if (args->host) {
+        printf("%12s: %s\n", "host", args->host);
+    }
+    if (args->port) {
+        printf("%12s: %d\n", "port", args->port);
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    int err = 0;
+
+    struct CliArgs args;
+    CliArgs_init(&args);
+    if (CliArgs_parse(&args, argc, argv) < 0) {
+        fprintf(stderr, "%s\n", HELP);
+        exit(EXIT_FAILURE);
+    }
+    CliArgs_debug(&args);
+
+    wolfSSL_Init();
+
+    return err;
 }
